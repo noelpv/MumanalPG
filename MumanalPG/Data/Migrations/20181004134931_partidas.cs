bce8ce643d4b6e2aec7dbde3828af036c9e96b91
Ronald Flores <r.flores.m@hotmail.com>
2018-10-04 09:53:43 -0400

    Migracion para Partidas

---
+++ MumanalPG/Data/Migrations/20181004134931_partidas.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MumanalPG.Data.Migrations
+{
+    public partial class partidas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "FinanzasParam");
+
+            migrationBuilder.CreateTable(
+                name: "Partida",
+                schema: "FinanzasParam",
+                columns: table => new
+                {
+                    IdPartida = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    IdSubGrupo = table.Column<short>(nullable: false),
+                    Descripcion = table.Column<string>(nullable: true),
+                    Gestion = table.Column<int>(nullable: false),
+                    VidaUtil = table.Column<short>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partida", x => x.IdPartida);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Partida",
+                schema: "FinanzasParam");
+        }
+    }
+}
