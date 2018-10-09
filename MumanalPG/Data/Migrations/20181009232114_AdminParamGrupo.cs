df25de9fb1509de8d54b44d9cc8edf169e7a2245
Ronald Flores <r.flores.m@hotmail.com>
2018-10-09 19:23:32 -0400

    Migration for AdminParamGroup

---
+++ MumanalPG/Data/Migrations/20181009232114_AdminParamGrupo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace MumanalPG.Data.Migrations
+{
+    public partial class AdminParamGrupo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "AdministraParam");
+
+            migrationBuilder.CreateTable(
+                name: "Grupo",
+                schema: "AdministraParam",
+                columns: table => new
+                {
+                    IdGrupo = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Descripcion = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupo", x => x.IdGrupo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grupo",
+                schema: "AdministraParam");
+        }
+    }
+}
