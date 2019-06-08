a1cf2bfd421425d0594f1269b771ec98166ae389
Ronald Flores <r.flores.m@hotmail.com>
2019-06-08 17:43:56 -0400

    [MUM-27]: Hoja de Ruta Interna(generacion de PDF)

---
+++ MumanalPG/Models/Correspondencia/DTO/HojaRutaPDFDTO.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace MumanalPG.Models.Correspondencia.DTO
+{
+    public class HojaRutaPDFDTO
+    {
+        public HojaRuta hojaRuta { get; set; }
+        public HojaRutaDetalle detalle { get; set; }
+        public List<Instrucciones> instrucciones { get; set; }
+        
+    }
+}
