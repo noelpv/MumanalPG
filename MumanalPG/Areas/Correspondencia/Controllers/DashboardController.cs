c0424acf8969666d46b11cc8a31b455b0521f298
Ronald <r.flores.m@hotmail.com>
2019-08-24 20:16:17 -0400

    remote dropdown (example)

--- MumanalPG/Areas/Correspondencia/Controllers/DashboardController.cs
+++ MumanalPG/Areas/Correspondencia/Controllers/DashboardController.cs
@@ -44,5 +44,32 @@ namespace MumanalPG.Areas.Correspondencia.Controllers
             //resp.RouteValue = new RouteValueDictionary {{ "filter", filter}};
             return View(resp);
         }
+
+
+        public IActionResult RemoteDropdown()
+        {
+            return View("RemoteDropdown");
+        }
+        
+        public JsonResult ListaUnidadesEjecutoras(string filter = "")
+        {
+            if (filter.Length > 2)
+            {
+                var unidades = DB.RRHH_UnidadEjecutora
+                    .Where(b => (b.IdEstadoRegistro != Constantes.Anulado || b.IdEstadoRegistro == null))
+                    .Where(b => EF.Functions.ILike(b.Descripcion, "%" + filter + "%") || EF.Functions.ILike(b.Sigla, "%" + filter + "%"))
+                    .OrderBy(d => d.Descripcion).Take(20)
+                    .Select(c => new {Id=c.IdUnidadEjecutora, Nombre = c.Descripcion, c.Sigla})
+                    .ToList(); 
+                
+                return Json(new {repositories = unidades});
+            }
+            else
+            {
+                return Json(new {repositories = new {}});
+            }
+        }
     }
+    
+    
 }
