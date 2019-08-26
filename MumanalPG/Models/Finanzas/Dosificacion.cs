1e6ba70e471897c3bcb043ce25bcba23210c9a5e
Orly <jeffdoom6@gmail.com>
2019-08-25 23:42:10 -0400

    [Modificacion de etiqueta de Dosificacion]

--- MumanalPG/Models/Finanzas/Dosificacion.cs
+++ MumanalPG/Models/Finanzas/Dosificacion.cs
@@ -23,7 +23,7 @@ namespace MumanalPG.Models.Finanzas
         public DateTime DosificacionFecha { get; set; }
         [Required(ErrorMessage = "{0} no puede estar en blanco")]
         [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
-        [Display(Name="Beneficiario")]
+        [Display(Name="CI-Beneficiario")]
         public Int32 IdBeneficiario { get; set; }
         public Int32 IdBeneficiarioResponsable { get; set; }
         [Required(ErrorMessage = "{0} no puede estar en blanco")]
