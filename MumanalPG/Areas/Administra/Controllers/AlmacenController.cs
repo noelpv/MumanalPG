2096cc4a42f666682dbc73c167350d4668e64cbd
Orly <jeffdoom6@gmail.com>
2019-09-04 23:13:55 -0400

    Almacen - Administra
    Co-Authored-By: jesussml <jesussml@users.noreply.github.com>

--- MumanalPG/Areas/Administra/Controllers/AlmacenController.cs
+++ MumanalPG/Areas/Administra/Controllers/AlmacenController.cs
@@ -1,98 +1,141 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
 using MumanalPG.Data;
-using MumanalPG.Models.Administra;
+using MumanalPG.Models;
+using MumanalPG.Utility;
+using ReflectionIT.Mvc.Paging;
+using SmartBreadcrumbs;
 
-namespace MumanalPG.Areas.Administra
+namespace MumanalPG.Areas.Administra.Controllers
 {
+    //[Authorize(Roles = SD.SuperAdminEndUser)]
+    [Authorize]
     [Area("Administra")]
-    public class AlmacenController : Controller
-    {
-        private readonly ApplicationDbContext _context;
-
-        public AlmacenController(ApplicationDbContext context)
+    public class AlmacenController : BaseController
+    {        
+        
+		public AlmacenController(ApplicationDbContext db, UserManager<IdentityUser> userManager): base(db, userManager)
         {
-            _context = context;
+            
         }
 
         // GET: Administra/Almacen
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Almacen.ToListAsync());
+        [Breadcrumb("Almacen", FromController = "DashboardAdministra", FromAction = "Clasificadores")]
+        public async Task<IActionResult> Index(string filter, int page = 1, string sortExpression = "IdTipoAlmacen", string a = "")
+        { 
+            var consulta = DB.Almacen.AsNoTracking().AsQueryable();
+            consulta = consulta.Include(m => m.TipoAlmacenDB)
+                               .Include(m => m.BeneficiarioDB)
+                               .Include(m => m.MunicipioDB)
+                               .Where(m => m.IdEstadoRegistro != 2);    //!= Constantes.Eliminado); // != el estado es diferente a ANULADO
+            if (!string.IsNullOrWhiteSpace(filter))
+			{
+                consulta = consulta.Where(m => EF.Functions.ILike(m.TipoAlmacenDB.Descripcion, $"%{filter}%"));
+            }
+            var resp = await PagingList.CreateAsync(consulta, Constantes.TamanoPaginacion, page, sortExpression, "IdTipoAlmacen");
+            resp.RouteValue = new RouteValueDictionary {{ "filter", filter}};
+            ShowFlash(a);
+            return View(resp);
         }
 
         // GET: Administra/Almacen/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(Int32? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var almacen = await _context.Almacen
-                .FirstOrDefaultAsync(m => m.IdAlmacen == id);
-            if (almacen == null)
+            var item = await DB.Almacen.Include(m => m.TipoAlmacenDB)
+                                    .Include(m => m.BeneficiarioDB)
+                                    .Include(m => m.MunicipioDB)
+                                    .Include(m => m.BarrioDB)
+                                    .Include(m => m.CalleDB)
+                                    .FirstOrDefaultAsync(m => m.IdAlmacen  == id);
+            
+            if (item == null)
             {
                 return NotFound();
             }
 
-            return View(almacen);
+            return PartialView("Details",item);
         }
 
         // GET: Administra/Almacen/Create
         public IActionResult Create()
         {
-            return View();
+            var model = new Models.Administra.Almacen();
+
+            // Datos para las dropdownlist's
+            ViewBag.TipoAlmacen = DB.TipoAlmacen.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Municipio = DB.Municipio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Barrio = DB.Barrio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Calle = DB.Calle.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+
+            return PartialView("Create", model);
         }
 
         // POST: Administra/Almacen/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdAlmacen,IdTipoAlmacen,Descripcion,IdBeneficiario,IdMunicipio,IdBarrio,IdCalle,NumeroEdificio,NumeroPiso,NumeroDepartamento,Telefono,Observacion,IdEstadoRegistro,IdUsuario,FechaRegistro")] Almacen almacen)
+        public async Task<IActionResult> Create(Models.Administra.Almacen item)
         {
             if (ModelState.IsValid)
             {
-                almacen.FechaRegistro = DateTime.Now;
-                almacen.IdEstadoRegistro = '1';
-
-                _context.Add(almacen);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                ApplicationUser currentUser = await GetCurrentUser();
+                item.IdUsuario = currentUser.AspNetUserId;
+                item.IdEstadoRegistro = 1;
+                item.FechaRegistro = DateTime.Now;
+                DB.Add(item);
+                await DB.SaveChangesAsync();
             }
-            return View(almacen);
+            // Datos para las dropdownlist's
+            ViewBag.TipoAlmacen = DB.TipoAlmacen.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Municipio = DB.Municipio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Barrio = DB.Barrio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Calle = DB.Calle.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            return PartialView("Create",item);
         }
 
         // GET: Administra/Almacen/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(Int32? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var almacen = await _context.Almacen.FindAsync(id);
-            if (almacen == null)
+            var item = await DB.Almacen
+                .Include(m => m.BeneficiarioDB)
+                .FirstOrDefaultAsync(m => m.IdAlmacen == id);
+            if (item == null)
             {
                 return NotFound();
             }
-            return View(almacen);
+            // Para editar, cargamos el nombre del beneficiario
+            item.NombreBeneficiario = item.BeneficiarioDB.Denominacion;
+
+            // Datos para las dropdownlist's
+            ViewBag.TipoAlmacen = DB.TipoAlmacen.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Municipio = DB.Municipio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Barrio = DB.Barrio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Calle = DB.Calle.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+
+            return PartialView("Edit", item);
         }
 
         // POST: Administra/Almacen/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdAlmacen,IdTipoAlmacen,Descripcion,IdBeneficiario,IdMunicipio,IdBarrio,IdCalle,NumeroEdificio,NumeroPiso,NumeroDepartamento,Telefono,Observacion,IdEstadoRegistro,IdUsuario,FechaRegistro")] Almacen almacen)
+        public async Task<IActionResult> Edit(Int32 id, Models.Administra.Almacen item)
         {
-            if (id != almacen.IdAlmacen)
+            if (id != item.IdAlmacen)
             {
                 return NotFound();
             }
@@ -101,12 +144,16 @@ namespace MumanalPG.Areas.Administra
             {
                 try
                 {
-                    _context.Update(almacen);
-                    await _context.SaveChangesAsync();
+                    ApplicationUser currentUser = await GetCurrentUser();
+                    item.IdUsuario = currentUser.AspNetUserId;
+                    item.IdEstadoRegistro = 1;
+                    item.FechaRegistro = DateTime.Now;
+                    DB.Update(item);
+                    await DB.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!AlmacenExists(almacen.IdAlmacen))
+                    if (!ItemExists(item.IdAlmacen))
                     {
                         return NotFound();
                     }
@@ -114,44 +161,69 @@ namespace MumanalPG.Areas.Administra
                     {
                         throw;
                     }
-                }
-                return RedirectToAction(nameof(Index));
+                }    
             }
-            return View(almacen);
+
+            // Datos para las dropdownlist's
+            ViewBag.TipoAlmacen = DB.TipoAlmacen.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Municipio = DB.Municipio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Barrio = DB.Barrio.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+            ViewBag.Calle = DB.Calle.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
+
+            return PartialView("Edit", item);
         }
 
         // GET: Administra/Almacen/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(Int32? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var almacen = await _context.Almacen
-                .FirstOrDefaultAsync(m => m.IdAlmacen == id);
-            if (almacen == null)
+            var item = await DB.Almacen.FirstOrDefaultAsync(m => m.IdAlmacen == id);
+            if (item == null)
             {
                 return NotFound();
             }
 
-            return View(almacen);
+            return PartialView("Delete",item);
         }
 
         // POST: Administra/Almacen/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(Int32 id)
         {
-            var almacen = await _context.Almacen.FindAsync(id);
-            _context.Almacen.Remove(almacen);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            var item = await DB.Almacen.FindAsync(id);
+            item.IdEstadoRegistro = 2;  //Constantes.Eliminado ;
+            DB.Almacen.Update(item);
+            await DB.SaveChangesAsync();
+            return PartialView("Delete",item);
         }
 
-        private bool AlmacenExists(int id)
+        private bool ItemExists(Int32 id)
         {
-            return _context.Almacen.Any(e => e.IdAlmacen == id);
+            return DB.Almacen.Any(e => e.IdAlmacen == id);
+        }
+
+        public JsonResult ListaBeneficiarios(string filter = "")
+        {
+            if (filter.Length > 2)
+            {
+                var listaBenef = DB.RRHH_Beneficiario
+                    .Where(b => (b.IdEstadoRegistro != Constantes.Anulado || b.IdEstadoRegistro == null))
+                    .Where(b => EF.Functions.ILike(b.Denominacion, "%" + filter + "%") || EF.Functions.ILike(b.DocumentoIdentidad, "%" + filter + "%"))
+                    .OrderBy(d => d.PrimerApellido).Take(20)
+                    .Select(c => new {Id=c.IdBeneficiario, Nombre = c.Denominacion, Carnet = c.DocumentoIdentidad})
+                    .ToList(); 
+                
+                return Json(new {repositories = listaBenef});
+            }
+            else
+            {
+                return Json(new {repositories = new {}});
+            }
         }
     }
 }
--- MumanalPG/Models/Administra/Almacen.cs
+++ MumanalPG/Models/Administra/Almacen.cs
@@ -1,25 +1,62 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using MumanalPG.Models.RRHH;
+using MumanalPG.Models.Generales;
 
 namespace MumanalPG.Models.Administra
 {
     [Table("Almacen", Schema = "Administra")]
     public class Almacen
     {
-        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public Int32 IdAlmacen { get; set; }
+        [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
+        [Display(Name="Tipo de Almacen")]
         public Int32 IdTipoAlmacen { get; set; }
+        public TipoAlmacen TipoAlmacenDB { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(50, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Descripción")]
         public string Descripcion { get; set; }
+        /* Beneficiario es llave foránea - INICIO */
+        [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
+        [Display(Name="Beneficiario")]
         public Int32 IdBeneficiario { get; set; }
+        // Con la siguiente linea obtenemos los datos de Denominación, Nombre, Apellido, CI, etc segun el IdBeneficiario
+        public Beneficiario BeneficiarioDB {get; set;}
+        // Para Editar, es necesario tener el nombre del beneficiario, de otro modo al editar el dropdownlist/ajax estara en blanco. Y como no pertenece a la base de datos, se la pone como "no mapeada"
+        [NotMapped]
+		public string NombreBeneficiario { get; set; }
+        /* Beneficiario es llave foránea - FIN*/
+        /* Municipio es llave foranea simple - INCIO (Tambien lo es Barrio y Calle)*/
+        [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
+        [Display(Name="Municipio")]
         public Int32 IdMunicipio { get; set; }
+        public Municipio MunicipioDB { get; set; }
+        /* Municipio es llave foranea simple - FIN*/
+        [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
+        [Display(Name="Barrio")]
         public Int32 IdBarrio { get; set; }
+        public Barrio BarrioDB { get; set; }
+        [Range(1, Int32.MaxValue, ErrorMessage = "Debe seleccionar un {0}")]
+        [Display(Name="Calle")]
         public Int32 IdCalle { get; set; }
+        public Calle CalleDB { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(10, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Nro. de Edificio")]
         public string NumeroEdificio { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(10, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Nro. de Piso")]
         public string NumeroPiso { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(10, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Nro. del Departamento")]
         public string NumeroDepartamento { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(40, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Teléfono")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage = "{0} no puede estar en blanco"), StringLength(200, ErrorMessage = "La longitud máxima es de {1} caracteres")]
+        [Display(Name="Observación")]
         public string Observacion { get; set; }
         public Int32 IdEstadoRegistro { get; set; }
         public Int32 IdUsuario { get; set; }
