7ae77a8533b272ec00083535ac0c11b21c01e727
Orly <jeffdoom6@gmail.com>
2019-09-21 18:53:21 -0400

    Revert "Clasificadores: Bienes"
    This reverts commit 7469eae64b43d69759d1586fdcb53e5e2a4af9cf.

--- MumanalPG/Areas/Administra/Controllers/BienesController.cs
+++ MumanalPG/Areas/Administra/Controllers/BienesController.cs
@@ -1,157 +1,98 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MumanalPG.Data;
-using MumanalPG.Models;
-using MumanalPG.Utility;
-using ReflectionIT.Mvc.Paging;
-using SmartBreadcrumbs;
+using MumanalPG.Models.Administra;
 
-namespace MumanalPG.Areas.Administra.Controllers
+namespace MumanalPG.Areas.Administra
 {
-    //[Authorize(Roles = SD.SuperAdminEndUser)]
-    [Authorize]
     [Area("Administra")]
-    public class BienesController : BaseController
-    {        
-        
-		public BienesController(ApplicationDbContext db, UserManager<IdentityUser> userManager): base(db, userManager)
+    public class BienesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BienesController(ApplicationDbContext context)
         {
-            
+            _context = context;
         }
 
         // GET: Administra/Bienes
-        [Breadcrumb("Bienes", FromController = "DashboardAdministra", FromAction = "Clasificadores")]
-        public async Task<IActionResult> Index(string filter, int page = 1, string sortExpression = "Descripcion", string a = "")
-        { 
-            var consulta = DB.Bienes.AsNoTracking().AsQueryable();
-            consulta = consulta.Include(m => m.UnidadMedidaDB).Where(m => m.IdEstadoRegistro != 2);    //!= Constantes.Eliminado); // != el estado es diferente a ANULADO
-                                                                        // .Include(m => m.PartidaGasto)
-                                                                        // Include(m => m.Descripcion)
-            if (!string.IsNullOrWhiteSpace(filter))
-			{
-                consulta = consulta.Where(m => EF.Functions.ILike(m.Descripcion, $"%{filter}%"));
-            }
-            var resp = await PagingList.CreateAsync(consulta, Constantes.TamanoPaginacion, page, sortExpression, "Descripcion");
-            resp.RouteValue = new RouteValueDictionary {{ "filter", filter}};
-            ShowFlash(a);
-            return View(resp);
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Bienes.ToListAsync());
         }
 
         // GET: Administra/Bienes/Details/5
-        public async Task<IActionResult> Details(Int32? id)
+        public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var item = await DB.Bienes.Include(m => m.PartidaGastoDB).Include(m => m.UnidadMedidaDB).FirstOrDefaultAsync(m => m.IdBienes  == id);
-            // .Include(m => m.PartidaGastoDB)            
-            // .Include(m => m.Descripcion)
-            if (item == null)
+            var bienes = await _context.Bienes
+                .FirstOrDefaultAsync(m => m.IdBienes == id);
+            if (bienes == null)
             {
                 return NotFound();
             }
 
-            return PartialView("Details",item);
+            return View(bienes);
         }
 
         // GET: Administra/Bienes/Create
         public IActionResult Create()
         {
-            var model = new Models.Administra.Bienes();
-
-            var items1 = DB.UnidadMedida.
-                Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
-            ViewBag.UnidadMedida = items1;
-
-            return PartialView("Create", model);
+            return View();
         }
 
         // POST: Administra/Bienes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Models.Administra.Bienes item)
+        public async Task<IActionResult> Create([Bind("IdBienes,IdPartidaGasto,CodigoBien,Descripcion,Observacion,IdUnidadMedida,IdUnidadMedidaEmpaque,IdModelo,IdPais,CodigoBienAnterior,DescripcionAnterior,IdRotacionBien,NombreArchivo,Foto,Kit,StockMinimo,StockInicial,StockIngreso,StockSalida,StockActual,PrecioReferenciaCompra,PrecioVentaBase,PrecioVentaFinal,TotalAcumuladoCompraBs,TotalAcumuladoVentaBs,TotalUtilidadBs,IdEstadoVigente,IdEstadoRegistro,IdUsuario,FechaRegistro")] Bienes bienes)
         {
             if (ModelState.IsValid)
             {
-                ApplicationUser currentUser = await GetCurrentUser();
-                item.IdUsuario = currentUser.AspNetUserId;
-                item.IdEstadoRegistro = 1;
-                item.IdEstadoVigente = 1;
-                item.FechaRegistro = DateTime.Now;
-                item.IdUnidadMedidaEmpaque = 0;
+                bienes.FechaRegistro = DateTime.Now;
+                bienes.IdEstadoRegistro = '1';
 
-                item.IdModelo = 0;
-                item.IdPais = 1;
-                item.CodigoBienAnterior = "";
-                item.DescripcionAnterior = "";
-                item.IdRotacionBien = 0;
-
-                item.PathArchivo = "";
-                item.Kit = 0;
-                item.StockMinimo = 0;
-                item.StockInicial = 0;
-                item.StockIngreso = 0;
-
-                item.StockSalida = 0;
-                item.StockActual = 0;
-                item.PrecioReferenciaCompra = 0;
-                item.PrecioVentaBase = 0;
-                item.PrecioVentaFinal = 0;
-
-                item.TotalAcumuladoCompraBs = 0;
-                item.TotalAcumuladoVentaBs = 0;
-                item.TotalUtilidadBs = 0;
-
-                DB.Add(item);
-                await DB.SaveChangesAsync();
+                _context.Add(bienes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
-            var items1 = DB.UnidadMedida.
-                Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
-            ViewBag.UnidadMedida = items1;
-
-            return PartialView("Create",item);
+            return View(bienes);
         }
 
         // GET: Administra/Bienes/Edit/5
-        public async Task<IActionResult> Edit(Int32? id)
+        public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-            var item = await DB.Bienes
-                .Include(m => m.PartidaGastoDB)
-                .FirstOrDefaultAsync(m => m.IdBienes == id);
-            if (item == null)
+
+            var bienes = await _context.Bienes.FindAsync(id);
+            if (bienes == null)
             {
                 return NotFound();
             }
-
-            item.NombrePartidaGasto = item.PartidaGastoDB.Descripcion;
-            //var itemsP = DB.PartidaGasto.
-            //    Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i => i.Descripcion).ToList();
-            //ViewBag.PartidaGasto = itemsP;
-
-            ViewBag.UnidadMedida = DB.UnidadMedida.Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i => i.Descripcion).ToList(); ;
-
-            return PartialView("Edit", item);
+            return View(bienes);
         }
 
         // POST: Administra/Bienes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Int32 id, Models.Administra.Bienes item)
+        public async Task<IActionResult> Edit(int id, [Bind("IdBienes,IdPartidaGasto,CodigoBien,Descripcion,Observacion,IdUnidadMedida,IdUnidadMedidaEmpaque,IdModelo,IdPais,CodigoBienAnterior,DescripcionAnterior,IdRotacionBien,NombreArchivo,Foto,Kit,StockMinimo,StockInicial,StockIngreso,StockSalida,StockActual,PrecioReferenciaCompra,PrecioVentaBase,PrecioVentaFinal,TotalAcumuladoCompraBs,TotalAcumuladoVentaBs,TotalUtilidadBs,IdEstadoVigente,IdEstadoRegistro,IdUsuario,FechaRegistro")] Bienes bienes)
         {
-            if (id != item.IdBienes)
+            if (id != bienes.IdBienes)
             {
                 return NotFound();
             }
@@ -160,41 +101,12 @@ namespace MumanalPG.Areas.Administra.Controllers
             {
                 try
                 {
-                    ApplicationUser currentUser = await GetCurrentUser();
-                    item.IdUsuario = currentUser.AspNetUserId;
-                    item.IdEstadoRegistro = 1;
-                    item.IdEstadoVigente = 1;
-                    item.FechaRegistro = DateTime.Now;
-                    item.IdUnidadMedidaEmpaque = 0;
-
-                    item.IdModelo = 0;
-                    item.IdPais = 1;
-                    item.CodigoBienAnterior = "";
-                    item.DescripcionAnterior = "";
-                    item.IdRotacionBien = 0;
-
-                    item.PathArchivo = "";
-                    item.Kit = 0;
-                    item.StockMinimo = 0;
-                    item.StockInicial = 0;
-                    item.StockIngreso = 0;
-
-                    item.StockSalida = 0;
-                    item.StockActual = 0;
-                    item.PrecioReferenciaCompra = 0;
-                    item.PrecioVentaBase = 0;
-                    item.PrecioVentaFinal = 0;
-
-                    item.TotalAcumuladoCompraBs = 0;
-                    item.TotalAcumuladoVentaBs = 0;
-                    item.TotalUtilidadBs = 0;
-
-                    DB.Update(item);
-                    await DB.SaveChangesAsync();
+                    _context.Update(bienes);
+                    await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ItemExists(item.IdBienes))
+                    if (!BienesExists(bienes.IdBienes))
                     {
                         return NotFound();
                     }
@@ -203,67 +115,43 @@ namespace MumanalPG.Areas.Administra.Controllers
                         throw;
                     }
                 }
-                
+                return RedirectToAction(nameof(Index));
             }
-            var items1 = DB.UnidadMedida.
-                Where(i => i.IdEstadoRegistro != Constantes.Anulado).OrderBy(i =>i.Descripcion).ToList();
-            ViewBag.UnidadMedida = items1;
-            return PartialView("Edit", item);
+            return View(bienes);
         }
 
         // GET: Administra/Bienes/Delete/5
-        public async Task<IActionResult> Delete(Int32? id)
+        public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var item = await DB.Bienes.FirstOrDefaultAsync(m => m.IdBienes == id);
-            if (item == null)
+            var bienes = await _context.Bienes
+                .FirstOrDefaultAsync(m => m.IdBienes == id);
+            if (bienes == null)
             {
                 return NotFound();
             }
 
-            return PartialView("Delete",item);
+            return View(bienes);
         }
 
         // POST: Administra/Bienes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Int32 id)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var item = await DB.Bienes.FindAsync(id);
-            item.IdEstadoRegistro = 2;  //Constantes.Eliminado ;
-            DB.Bienes.Update(item);
-            await DB.SaveChangesAsync();
-            return PartialView("Delete",item);
+            var bienes = await _context.Bienes.FindAsync(id);
+            _context.Bienes.Remove(bienes);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
 
-        private bool ItemExists(Int32 id)
+        private bool BienesExists(int id)
         {
-            return DB.Bienes.Any(e => e.IdBienes == id);
-        }
-
-        public JsonResult ListaPartidaGastos(string filter = "")
-        {
-            if (filter.Length > 2)
-            {
-                // || EF.Functions.ILike(b.IdPartidaGasto, "%" + filter + "%")
-                // || b.IdEstadoRegistro == null)
-                var listaBenef = DB.PartidaGasto
-                    .Where(b => (b.IdEstadoRegistro != Constantes.Anulado ))
-                    .Where(b => EF.Functions.ILike(b.Descripcion, "%" + filter + "%") )
-                    .OrderBy(d => d.Descripcion).Take(20)
-                    .Select(c => new {Id=c.IdPartidaGasto, Nombre = c.Descripcion})
-                    .ToList(); 
-                
-                return Json(new {repositories = listaBenef});
-            }
-            else
-            {
-                return Json(new {repositories = new {}});
-            }
+            return _context.Bienes.Any(e => e.IdBienes == id);
         }
     }
 }
