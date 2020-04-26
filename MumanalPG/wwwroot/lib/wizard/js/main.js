63d4d2ca6072ec516c664ff7c494d5ee71256ac8
Ronald <rfm@iventiva.com>
2020-04-26 11:19:20 -0400

    Fixes

--- MumanalPG/wwwroot/lib/wizard/js/main.js
+++ MumanalPG/wwwroot/lib/wizard/js/main.js
@@ -27,11 +27,43 @@ $(document).ready(function() {
         },
         onStepChanging: function(event, currentIndex, newIndex) {
             // if (currentIndex === 0) {
-            //     form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+            //     // form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+            //     alert('1');
             // }
-            // if (currentIndex > 1) {
-            //     $('a[role="menuitem"]').addClass('disabled');
-            // }
+
+            if (currentIndex === 1) {
+                let id_fun, inst_fun, inst_value;
+                let is_valid = false;
+                let errors = [];
+                $('.derive-selected').each(function() {
+                    let me = $(this);
+                    id_fun = me.attr('id');
+                    inst_fun = $(`input#ins_${id_fun}`);
+                    if(inst_fun.length && inst_fun.val()) {
+                        inst_value = JSON.parse(inst_fun.val());
+                        if(inst_value.instrucciones.length === 0) {
+                            errors.push(id_fun);
+                            me.addClass('derive-selected-error');
+                        } else {
+                            me.removeClass('derive-selected-error');
+                        }
+                    }
+                });
+                if(errors.length > 0) {
+                    let msg_error = $('<div id="errors" class="text-danger text-center mb-3">Los usuarios en rojo necesitan instrucciones.</div>');
+                    $('#errors').remove();
+                    $('.actions').first().prepend(msg_error);
+                } else if($('.derive-selected').length){
+                    $('#errors').remove();
+                    is_valid = true;
+                } else {
+                    let msg_error = $('<div id="errors" class="text-danger text-center mb-3">Debe seleccionar almenos a una persona para derivar.</div>');
+                    $('#errors').remove();
+                    $('.actions').first().prepend(msg_error);
+                }
+                return is_valid;
+            }
+            
             // if (currentIndex === 2) {
             //     form.parent().parent().parent().find('.footer').removeClass('footer-1').addClass('footer-' + currentIndex + '');
             // }
@@ -45,36 +77,47 @@ $(document).ready(function() {
             return form.valid();
         },
         onFinishing: function(event, currentIndex) {
-            form.validate().settings.ignore = ":disabled";
-            return form.valid();
+            let row_value, container_row, container_input;
+            let anexo_errors = [];
+            $('[id*="row_anexo_"]').each(function() {
+                if($(this).val()){
+                    row_value = JSON.parse($(this).val());
+                    container_row = $(this).parents('div').first();
+                    if(!(row_value.descripcion != null && row_value.descripcion.length)){
+                        container_input = container_row.find('[id*="descripcion_"]').first().parents('div.form-group').first();
+                        container_input.addClass('has-danger');
+                        anexo_errors.push(container_row.find('[id*="descripcion_"]').first().attr('id'));
+                    } else {
+                        container_input = container_row.find('[id*="descripcion_"]').first().parents('div.form-group').first();
+                        container_input.removeClass('has-danger');
+                    }
+                    
+                    if(!(row_value.path != null && row_value.path.length)){
+                        container_input = container_row.find('[id*="dropzone_"]').first().parents('div.form-group');
+                        container_input.addClass('has-danger');
+                        anexo_errors.push(container_row.find('[id*="dropzone_"]').first().attr('id'));
+                    }else {
+                        container_input = container_row.find('[id*="dropzone_"]').first().parents('div.form-group').first();
+                        container_input.removeClass('has-danger');
+                    }
+                }
+            });
+
+            if(anexo_errors.length > 0) {
+                let msg_error = $('<div id="errors" class="text-danger text-center mb-3">Corrija los campos en rojo.</div>');
+                if( !$('#errors').length) {
+                    $('.actions').first().prepend(msg_error);
+                }
+            } else {
+                $('#errors').remove();
+            }
+            return !anexo_errors.length > 0;
         },
         onFinished: function(event, currentIndex) {
             $(".preloader").fadeIn();
             form.submit();
         },
         onStepChanged: function(event, currentIndex, priorIndex) {
-            if(currentIndex === 1) {
-                var is_valid = false;
-
-                $('[id*="ins_fun_"]').each(function() {
-                    if($(this).val()){
-                        var ins_value = JSON.parse($(this).val());
-                        if(ins_value.instrucciones.length === 0) {
-                            is_valid = false;
-                            return false;
-                        } else {
-                            is_valid = true;
-                        }
-                    }
-                });
-                if(is_valid) {
-                    $('a[role="menuitem"]').removeClass('disabled');
-                } else {
-                    $('a[role="menuitem"]').addClass('disabled');
-                }
-            } else {
-                $('a[role="menuitem"]').removeClass('disabled');
-            }
             return true;
         }
     });
